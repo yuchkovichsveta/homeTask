b75e9f11fb71ebb9b487e4f6a7ddac497b7b4db2
yuchkovichsveta <yuchkovichsveta@gmail.com>
2018-03-31 09:42:47 +0300

    added task4.2.js  to project

--- task 4/task4.2.js
+++ task 4/task4.2.js
@@ -1,12 +1,13 @@
 
 var phStr1 = "+375 29 300-20-20";
-var phStr2 = "+375(29)300-20-20";
-var phStr2 = "+37544300 20 20";
-var phStr2 = "+37533 3002020";
+var phStr2 = "35(29)300-20-20";
+var phStr3 = "+37544 23120 20";
+var phStr4 = "+533 3002020";
 // phStr1 = +prompt("Enter phoneNumber");
 // phStr2 = +prompt("Enter phoneNumber2");
 
-var refExp = /\+?375[\s(]?29|3{2}|4{2}[)\s]?\d{3}[\s-]?\d{2}/;
+var refExp = /\s*\+?375[\s ]?(\(?29\)?)|(\(?3{2}\)?)|(\(?4{2}\)?)[\s ]?\d{3}[\s- ]?\d{2}/;
+//var refExp = /\+?3{1}7{1}5{1}[\s(]?29|3{2}|4{2}[)\s]?\d{3}[\s-]?\d{2}/;
 
 //var equality = false;
 
@@ -24,4 +25,19 @@ if (refExp.test(phStr2)){
 
 else {
 	document.write(phStr2 + " is a ircorrect phone Number  <br \/>");
+}
+
+if (refExp.test(phStr3)){
+	document.write(phStr3 + " is a correct phone Number  <br \/>");
+}
+
+else {
+	document.write(phStr3 + " is a ircorrect phone Number  <br \/>");
+}
+if (refExp.test(phStr4)){
+	document.write(phStr4 + " is a correct phone Number  <br \/>");
+}
+
+else {
+	document.write(phStr4 + " is a ircorrect phone Number  <br \/>");
 }
