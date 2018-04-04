2103548abf25c530e3c3b313ece3c009b98a5cf1
yuchkovichsveta <yuchkovichsveta@gmail.com>
2018-04-04 13:32:44 +0300

    changed tasks4.2js

--- task 4/task4.2.js
+++ task 4/task4.2.js
@@ -1,22 +1,23 @@
-var phStr0 = +prompt("Enter your phoneNumber");
+var phStr0 = prompt("Enter your phoneNumber");
 var phStr1 = "  +375 44 370 2030";
 var phStr2 = " 37329300-20-20";
 var phStr3 = "+373 (25)233 14 20";
 var phStr4 = "  +375 29 3003390";
 var phArr = [phStr1, phStr2, phStr3, phStr4];
 
-var regExp = /^\s*\+?375([\s]?((29|33|44)|(\((44|29|33)\)))[\s]?)\s?\d{3}[\s-]?\d{2}[\s-]?\d{2}$/;
-
+//var regExp = /^\s*\+?375([\s]?((29|33|44)|(\((44|29|33)\)))[\s]?)\s?\d{3}[\s-]?\d{2}[\s-]?\d{2}$/;
+var regExp =/^\s*\+?\s*375\s*\(?(25|29|33|44)\)?\s?\d{3}[\s-]?\d{2}[\s-]?\d{2}/
 
 document.write(" Info about your phoneNumber: <br \/>");
 
-if (regExp.test(phStr0)) {
-	document.write("your phoneNumber: " + phStr0 + " is correct <br \/>");
-}
 
-else {
-	document.write("your phoneNumber: " + phStr0 + " is ircorrect <br \/>");
-}
+	if ( phStr0.match(regExp)!=null){
+		document.write("your phoneNumber: " + phStr0 + " is correct <br \/>");
+	}
+
+	else {
+		document.write("your phoneNumber: " + phStr0 + " is ircorrect <br \/>");
+	}
 
 
 document.write(" <br \/> My tests of phoneNumbers: <br \/>");
