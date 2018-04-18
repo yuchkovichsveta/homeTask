932911f8eaa86f1b4c9697602850e6f7910470c2
yuchkovichsveta <yuchkovichsveta@gmail.com>
2018-04-18 14:28:13 +0300

    added task 8.2

---
+++ task 8/task 8.2.js
@@ -0,0 +1,11 @@
+var rect = document.getElementById('rect');
+
+setInterval(function() {
+  if(rect.getBoundingClientRect().top < 40) {
+    rect.classList.add('fix');
+  }
+  if(window.scrollY > 12000){
+    rect.classList.remove('fix');
+  }
+
+}, 1);
