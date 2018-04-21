12635070c3cd08b818cc1630a53cf556e217c1de
yuchkovichsveta <yuchkovichsveta@gmail.com>
2018-04-21 23:06:23 +0300

    changed task 10.2

--- taskt 10/task 10.2.js
+++ taskt 10/task 10.2.js
@@ -7,11 +7,49 @@ var left = document.getElementById("left");
 var right = document.getElementById("right");
 var center = document.getElementById("center");
 
+var link = document.getElementById("link");
+var unlink = document.getElementById("unlink");
+
 
 boldBtn.addEventListener("click", function(e){ // work
 	document.execCommand("bold",false,null);
 })
 
+undLineBtn.addEventListener("click", function(e){ // work
+	document.execCommand("underline",false,null);
+})
+
+italicBtn.addEventListener("click", function(e){ // work
+	document.execCommand("italic",false,null);
+})
+
+decoration.addEventListener("click", function(e){ // work
+	document.execCommand("strikeThrough",false,null);
+})
+
+left.addEventListener("click", function(e){ // work
+	document.execCommand("justifyleft",false,null);
+})
+
+right.addEventListener("click", function(e){ // work
+	document.execCommand("justifyright",false,null);
+})
+
+center.addEventListener("click", function(e){ // work
+	document.execCommand("justifycenter",false,null);
+})
+
+link.addEventListener("click", function(e){ // work
+	var mylink = prompt("Enter a URL:", "http://");
+	if ((mylink != null) && (mylink != "")) {
+		document.execCommand("createlink",false,mylink);
+	}
+	
+})
+
+unlink.addEventListener("click", function(e){ // work
+	document.execCommand("unlink",false,null);
+})
 
 setInterval(function(){
 	if(!document.queryCommandEnabled("bold")){
@@ -25,9 +63,6 @@ setInterval(function(){
 }
 },100)
 
-italicBtn.addEventListener("click", function(e){ // work
-	document.execCommand("italic",false,null);
-})
 
 
 setInterval(function(){
@@ -42,11 +77,9 @@ setInterval(function(){
 	else{
 		italicBtn.style.fontStyle = "normal";
     }
- })
+ },100)
 
-undLineBtn.addEventListener("click", function(e){ // work
-	document.execCommand("underline",false,null);
-})
+
 
 setInterval(function(){
 	if(!document.queryCommandEnabled("underline")) {
@@ -62,11 +95,8 @@ setInterval(function(){
 		undLineBtn.style.fontWeight = "normal";
 		undLineBtn.style.textDecoration = "none";
     }
- })
+ },100)
 
-decoration.addEventListener("click", function(e){ // work
-	document.execCommand("strikeThrough",false,null);
-})
 
 setInterval(function(){
 	if(!document.queryCommandEnabled("strikeThrough")) {
@@ -82,24 +112,82 @@ setInterval(function(){
 		decoration.style.fontWeight = "normal";
 		decoration.style.textDecoration = "none";
     }
- })
+ },100)
 
-left.addEventListener("click", function(e){ // work
-	document.execCommand("justifyleft",false,null);
-})
 
-/*
+
+
 setInterval(function(){
 	if(!document.queryCommandEnabled("justifyleft")) {
 		left.disabled = true;
 	} else {
 		left.disabled = false;
 	}
-	if (document.queryCommandState("strikeThrough")) {
+	if (document.queryCommandState("justifyleft")) {
 		left.style.fontWeight = "bold";
 	} 
 	else{
 		left.style.fontWeight = "normal";
     }
- })*/
+ },100)
 
+
+
+setInterval(function(){
+	if(!document.queryCommandEnabled("justifyright")) {
+		right.disabled = true;
+	} else {
+		right.disabled = false;
+	}
+	if (document.queryCommandState("justifyright")) {
+		right.style.fontWeight = "bold";
+	} 
+	else{
+		right.style.fontWeight = "normal";
+    }
+ },100)
+
+
+
+setInterval(function(){
+	if(!document.queryCommandEnabled("justifycenter")) {
+		center.disabled = true;
+	} else {
+		center.disabled = false;
+	}
+	if (document.queryCommandState("justifycenter")) {
+		center.style.fontWeight = "bold";
+	} 
+	else{
+		center.style.fontWeight = "normal";
+    }
+ },100)
+
+
+setInterval(function(){
+	if(!document.queryCommandEnabled("createlink")) {        
+		   link.disabled = true;
+	} else {
+		link.disabled = false;
+	}
+	if (document.queryCommandState("createlink")) {
+		link.style.fontWeight = "bold";
+	} 
+	else{
+		link.style.fontWeight = "normal";
+    }
+ },100)
+
+setInterval(function(){
+	if(!document.queryCommandEnabled("unlink")) {        
+		   unlink.disabled = true;
+	} else {
+		unlink.disabled = false;
+	}
+	if (document.queryCommandState("unlink")) {
+		unlink.style.fontWeight = "bold";
+	} 
+	else{
+		unlink.style.fontWeight = "normal";
+    }
+ },100)
