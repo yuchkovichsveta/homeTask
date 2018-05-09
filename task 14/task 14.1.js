3fa11540029eb7f19c33bcf383c884126870398a
yuchkovichsveta <yuchkovichsveta@gmail.com>
2018-05-09 22:45:31 +0300

    solved task 14.5, 14.7, in print method used

--- task 14/task 14.1.js
+++ task 14/task 14.1.js
@@ -1,7 +1,7 @@
 var div = document.getElementById("idDiv");
 
-class Task{
-    constructor(name, priority, endDate){
+class Task {
+    constructor(name, priority, endDate) {
       this.name = name;
       this.priority = priority;
       this.endDate = endDate;
@@ -30,16 +30,26 @@ class TaskPlan {
        return newtaskPlan;
     }
 
+     addTaskArr(...arrTask){
+      console.log(`Added ${arrTask.length} tasks.`);
+     
+      for(var i = 0; i < arrTask.length; i++){
+          this.taskPlan.push(arrTask[i]);
+          console.log(arrTask[i]);
+         
+      }
+    
+   }
      deleteTask(taskIndex) {
         var newtaskPlan = this.taskPlan.splice(taskIndex, 1);
         return  this.taskPlan;
     }
 
-     printTaskPlan() {
-      
-       document.write("<b>name|</b> " + " <b> priority| </b>" + " <b> endDate </b> <br></br> ");    
+     printTaskPlan() { 
+       document.write("<b>name|</b> " + " <b> priority| </b>" + " <b> endDate </b> <br></br> ");     
        for (var i = 0; i < this.taskPlan.length; i++) {
-         document.write(this.taskPlan[i].name + " | " + this.taskPlan[i].priority + " | " + this.taskPlan[i].endDate + "<br></br>")
+         document.write(`${this.taskPlan[i].name} | ${this.taskPlan[i].priority} | ${this.taskPlan[i].endDate} <br></br>`);
+        
        } 
      }
 
@@ -103,41 +113,132 @@ class TaskPlan {
        
 }
 
+class TaskList {
+
+    constructor(...taskList) {
+        this.taskList = taskList;
+    }
+
+    addTaskToList(task){
+        this.taskList.push(task); 
+    }
+  
+   addArrTaskToList(...arrTask){
+      console.log(`Added ${arrTask.length} tasks`);
+     
+      for(var i = 0; i < arrTask.length; i++) {
+          this.taskList.push(arrTask[i]);
+          console.log(arrTask[i]);
+         
+      }
+    
+  }
+
+  printTaskList() { 
+    document.write("<b>name|</b> " + " <b> priority| </b>" + " <b> endDate </b> <br></br> ");     
+      for (var i = 0; i < this.taskList.length; i++) {
+        document.write(`${this.taskList[i].name} | ${this.taskList[i].priority} | ${this.taskList[i].endDate} <br></br>`);
+        
+      } 
+  }
+
+  deleteTaskInList(task) {
+    document.write(`${task.name} deleted`);  
+    this.taskList.splice(task, 1);
+  }
+}
+
+function ajaxGet(path) {
+  var promise = new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest; 
+    xhr.open("GET", path, true);
+
+    xhr.onload = function() { 
+      resolve(xhr.responseText); 
+    } 
+
+    xhr.onerror = function() {
+      reject(xhr.responseText);
+    }
+
+    xhr.send(null); 
+  })
+  return promise;
+}
+
+ajaxGet("1.json").then(function(text) { 
+  var data = JSON.parse(text);
+  console.log(data.length);
+  var list = new Set();
+  var plan = new Set();
+
+  for(var i = 0; i < data.length; i++) {
+    var task = new Task(data[i].name,data[i].priority,data[i].endDate);
+
+    if(data[i].endDate == null) {
+      list.add(task);
+    }
+
+    else {
+      plan.add(task);
+    }
+  }
+
+  
+  
+  taskPlan.addTaskArr(...plan);
+  document.write("Testing of Set (task 1.5) <b> taskPlan </b> with exact endDate in 1.json (task 1.7) <br></br> ");    
+  console.log(taskPlan);
+  taskPlan.printTaskPlan();
+
+  document.write("Testing of Set (task 1.5) <b> taskList </b> without exact endDate in 1.json (task 1.7) <br></br> ");       
+
+  taskList.addArrTaskToList(...list);
+  console.log( taskList);
+  taskList.printTaskList();
+  
+  
+},err=>console.error(err));
+
+var taskList = new TaskList();
+var taskPlan = new TaskPlan();
+
 var now = new Date();
 var tommorow = new Date();
 tommorow.setTime(tommorow.getTime()+1000*60*60*24);
 
-var task1 = new Task("task1", 1, new Date(now.getFullYear(), now.getMonth(), now.getDate()));
-console.log(task1);
+/*var task1 = new Task("task1", 1, new Date(now.getFullYear(), now.getMonth(), now.getDate()));
+//console.log(task1);
 var task2 = new Task("task2", 4, new Date(now.getFullYear(), now.getMonth(), now.getDate()));
 var task3 = new Task("task3", 2, new Date(tommorow.getFullYear(), tommorow.getMonth(), tommorow.getDate()));
-console.log(task3);
+//console.log(task3);
 var task4 = new Task("task4", 3, new Date(tommorow.getFullYear(), tommorow.getMonth(), tommorow.getDate()));
-console.log(task4);
+//console.log(task4);
 var task5 = new Task("task5", 1, new Date(now.getFullYear(), now.getMonth(), now.getDate()));
 var task6 = new Task("task6", 2, new Date(tommorow.getFullYear(), tommorow.getMonth(), tommorow.getDate()));
 
 var arr = new Array(task1, task2, task3, task4, task6);
-var arr1 = [task3, task2];
+var arr1 = [task3, task2];*/
+var arr = [];
 var taskPlan = new TaskPlan(arr);
 var taskPlan2 = new TaskPlan(arr);
 
 document.write("Initial taskPlan: <br></br> ");
 
-taskPlan.printTaskPlan();
+//taskPlan.printTaskPlan();
 
 document.write("taskPlan after addition of task5: <br></br> ");
 
 taskPlan.addTask(task5);
-taskPlan.printTaskPlan();
+//taskPlan.printTaskPlan();
 
 document.write("taskPlan after sort by priority: <br></br> ");
 taskPlan.getListByPriority();
-taskPlan.printTaskPlan();
+//taskPlan.printTaskPlan();
 
 document.write("taskPlan after deleted task with index = 2 : <br></br> ");
 taskPlan.deleteTask(2);
-taskPlan.printTaskPlan();
+//taskPlan.printTaskPlan();
 
 document.write("taskPlan for today: <br></br> ");
 taskPlan.getTasksForToday();
@@ -147,13 +248,13 @@ taskPlan.getTasksForTommorow();
 
 document.write("taskPlan after using SPREAD (x = task2, ...arr = [task3, task2]): <br></br> ");
 taskPlan.addTaskArr(task2, ...arr1);
-taskPlan.printTaskPlan();
+//taskPlan.printTaskPlan();
 
 document.write("taskPlan after using SPREAD (x = task1, ...arr = [task4, task5, task3]): <br></br> ");
 var arr1 = [task4, task3, task5];
-taskPlan.addTaskArr(task1, ...arr1);
-taskPlan.printTaskPlan();
+taskPlan.addTaskArr(...arr1);
+//taskPlan.printTaskPlan();
 
 document.write("testing of iterator  <br></br> ");
-taskPlan.getTasksForTodayUsingIterator();
+//taskPlan.getTasksForTodayUsingIterator();
 
